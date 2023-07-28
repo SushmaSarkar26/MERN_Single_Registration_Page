fd1460c20ddd5fa0c336b1bbdb983b153194f24e
Sushma <sushmasarkar2004@gmail.com>
2023-07-29 03:24:50 +0530

    database connection

--- server/database/db.js
+++ server/database/db.js
@@ -15,4 +15,3 @@ const Connection = async () => {
 
 
 Connection();
-// export default Connection;
--- server/index.js
+++ server/index.js
@@ -1,22 +1,12 @@
 const dotenv = require("dotenv");
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 require('./database/db');
-// import Connection from "./database/db";
 
 dotenv.config({ path: './.env'});
 
 
-// const Connection = async () => {
-//     const URL = process.env.DATABASE;
-//     try {
-//         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true});
-//         console.log('Dtabase connected Successfully')
-//     } catch (error) {
-//         console.log('Error while connecting with the database', error.message);
-//     }
-// }
 
 // middleware
 const middleware = (req, res, next) => {
