57606c0287eaacd67c52cc5297e0cb40d9af2f65
Sushma <sushmasarkar2004@gmail.com>
2023-07-29 01:52:53 +0530

    database Connection

---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env'});
+
+const Connection = async () => {
+    const URL = process.env.DATABASE;
+    try {
+        await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true});
+        console.log('Dtabase connected Successfully')
+    } catch (error) {
+        console.log('Error while connecting with the database', error.message);
+    }
+}
+
+
+Connection();
+// export default Connection;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,42 @@
+const dotenv = require("dotenv");
+// const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+require('./database/db');
+// import Connection from "./database/db";
+
+dotenv.config({ path: './.env'});
+
+
+// const Connection = async () => {
+//     const URL = process.env.DATABASE;
+//     try {
+//         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true});
+//         console.log('Dtabase connected Successfully')
+//     } catch (error) {
+//         console.log('Error while connecting with the database', error.message);
+//     }
+// }
+
+// middleware
+const middleware = (req, res, next) => {
+    console.log(`Hello my Middleware`);
+    next();
+}
+
 
 app.get('/', (req, res) => {
     res.send(`Hello from the server`)
 });
 
+app.get('/about', middleware, (req, res) => {
+    res.send(`Hello from the about page`)
+});
 
-app.listen(8000, () => {
+
+// Connection();
+
+
+app.listen(process.env.PORT, () => {
     console.log('server connected')
 });
